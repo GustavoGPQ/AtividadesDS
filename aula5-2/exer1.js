c082dc7ff48cd801bcb8a2e5828e878c9ce5dd04
GustavoGPQ <gustavoporto0711@gmail.com>
2024-09-18 13:50:08 -0300

    aula 5-2

---
+++ aula5-2/exer1.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")()
+
+let vetor = [1, 2, 3];
+let vetorMultiplicado = vetor.map(x => x * 2);
+console.log(vetorMultiplicado);
---
+++ aula5-2/exer2.js
@@ -0,0 +1,4 @@
+let vetorAleatorio = [42, 7, 19, 3, 58];
+let vetorOrdenado = vetorAleatorio.slice().sort((a, b) => a - b);
+console.log("Vetor original:", vetorAleatorio);
+console.log("Vetor ordenado:", vetorOrdenado);
